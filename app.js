0cf199d0f69210a924c49e5e232be55c9afc358a
DasaGott <142871686+DasaGott@users.noreply.github.com>
2023-10-27 17:16:32 +0200

    Added search button to be interactive

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const openElement =
   document.getElementById("open");
 const closeElement =
   document.getElementById("close");
+
 const changeTheme =
   document.querySelector(".container");
 openElement.addEventListener("click", () =>
@@ -10,3 +11,15 @@ openElement.addEventListener("click", () =>
 closeElement.addEventListener("click", () =>
   changeTheme.classList.remove("show-nav")
 );
+
+let searchInput =
+  document.querySelector("#search");
+
+let searchIcon = document.querySelector(
+  "#search-button"
+);
+
+searchIcon.addEventListener("click", () => {
+  searchIcon.classList.toggle("active");
+  searchInput.classList.toggle("active");
+});
